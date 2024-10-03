329265011dc7d9fd312206ec5e55a7b8d84dfeb0
git.filipequeiroz <filipe.queiroz@opovodigital.com>
2024-10-03 17:42:55 -0300

    primeiro

---
+++ api-livros/controllers/livrosController.js
@@ -0,0 +1,53 @@
+import { db } from "../db.js";
+
+export const getLivros = (_, res) => {
+  const sql = "SELECT * FROM livros";
+
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data);
+  });
+};
+
+export const addLivro = (req, res) => {
+  const sql =
+    "INSERT INTO livros(`livro`, `data`) VALUES(?)";
+
+  const values = [
+    req.body.livro,
+    req.body.data,
+  ];
+
+  db.query(sql, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Livro adicionado");
+  });
+};
+
+export const updateLivro = (req, res) => {
+  const sql =
+    "UPDATE livros SET `livro` = ?, `data` = ? WHERE `id` = ?";
+
+  const values = [
+    req.body.livro,
+    req.body.data,
+  ];
+
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Livro atualizado com sucesso.");
+  });
+};
+
+export const deleteLivro = (req, res) => {
+  const q = "DELETE FROM livros WHERE `id` = ?";
+
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(" deletado com sucesso.");
+  });
+};
---
+++ api-livros/routes/livros.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { addLivro, deleteLivro, getLivros, updateLivro } from "../controllers/livrosController.js";
+
+const router = express.Router()
+
+router.get("/", getLivros)
+
+router.post("/", addLivro)
+
+router.put("/:id", updateLivro)
+
+router.delete("/:id", deleteLivro)
+
+export default router
